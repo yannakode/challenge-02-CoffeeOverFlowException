f5510aa436fae46440095930b48dd6ba9120f694
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2023-10-11 14:12:54 -0300

    refactor[annotation]: NoArgsConstructor annotation added

--- product-service/src/main/java/com/compassuol/sp/challenge/msproducts/model/dto/ProductResponseDto.java
+++ product-service/src/main/java/com/compassuol/sp/challenge/msproducts/model/dto/ProductResponseDto.java
@@ -2,11 +2,13 @@ package com.compassuol.sp.challenge.msproducts.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponseDto {
     private Long id;
     private String name;
